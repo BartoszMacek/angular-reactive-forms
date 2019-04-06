205418d93fd043075937ab8900b4927c8d5e4102
Bartosz Macek <bartoszmacek@gmail.com>
2019-04-06 16:01:17 +0100

    Reactive forms in Angular / Adding css (creating forms in TimeScript)

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.email, Validators.required]),
+      'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
